f0a849f7cab62069e9a7622195ed7952704be7c3
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-03-05 20:57:47 -0500

    a6 changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Ewq from "./ques4"
+import Asd from "./21"
+
+import Xyz from "./comp";
+import Jkl from "./10"
+
+import Xyz1 from "./17"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>Hello</h1>
+
+<Jkl/>
+        <Xyz/>
+        <Ewq/>
+
+      <Asd/>
+
+        <Xyz1/>
+
+
+
     </div>
   );
 }
