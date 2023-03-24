c409a6051b277d8d443392babafc81b850fa5317
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-03-24 11:54:26 -0400

    a7 changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Route,Routes} from "react-router";
+
+import TodoItem from "./todo/todo-item";
 import Exam from "./exam";
 
 
 
 function App() {
+
+    var str= "Here is a link<a href='#'>Link</a>"
   return (
 
     <div className="Container">
+
+
+
         <BrowserRouter>
 
 
@@ -21,12 +28,14 @@ function App() {
                 <Route index element={<Labs/>}/>
                 <Route path="/hello" element={<HelloWorld/>}/>
                 <Route path="/tuiter/*" element={<Tuiter/>}/>
+                <Route path="/todo" element={<TodoItem/>}/>
                 <Route path="/exam"
                        element={<Exam/>}/>
             </Routes>
 
 
         </BrowserRouter>
+
 
 
     </div>
