df0ab7c229a71a11648414a0d895e410b6580be8
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-03-26 14:40:11 -0400

    a7 changes

---
+++ src/labs/a7/redux-examples/reducers/todos-reducer.js
@@ -0,0 +1,47 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialTodos = [
+    {
+        _id: "123",
+        do: "Accelerate the world's transition to sustainable energy",
+        done: false
+    },
+    {
+        _id: "234",
+        do: "Reduce space transportation costs to become a spacefaring civilization",
+        done: false
+    },
+];
+
+const todosSlice = createSlice({
+                                   name: 'todos',
+                                   initialState: initialTodos,
+                                   reducers: {
+                                       addTodo(state, action) {
+                                           state.push({
+                                                          _id: (new Date()).getTime(),
+                                                          do: action.payload.do,
+                                                          done: false
+                                                      });
+                                       },
+                                       deleteTodo(state,action) {
+                                           const index = action.payload
+                                           console.log(index)
+
+                                        state.splice(index,1)
+                                           console.log(state)
+
+                                       }
+                                       ,
+                                       updateTodo(state,action) {
+                                           const index = action.payload
+                                           console.log(index)
+                                           state[index].done  =  state[index].done ? false : true
+
+                                       }
+
+                                   }});
+
+export const {addTodo,deleteTodo,updateTodo} = todosSlice.actions
+export default todosSlice.reducer
+
---
+++ src/labs/a7/redux-examples/todos-component.js
@@ -0,0 +1,79 @@
+import React, {useState,useEffect} from "react";
+
+import {useDispatch,useSelector} from "react-redux";
+import {addTodo}
+    from "./reducers/todos-reducer";
+import {deleteTodo}
+    from "./reducers/todos-reducer";
+import {updateTodo}
+    from "./reducers/todos-reducer";
+
+
+const Todos = () => {
+    const todos
+        = useSelector(state => state.todos);
+    const [todo, setTodo] = useState({do: 'Hey'});
+    const dispatch = useDispatch();
+    const createTodoClickHandler = () => {
+        dispatch(addTodo(todo))
+    }
+
+    const deleteTodoClickHandler = (_id) => {
+
+        dispatch(deleteTodo(_id))
+        console.log("Log",_id)
+
+    }
+
+    function toggleTodo(index){
+        dispatch(updateTodo(index))
+    }
+
+
+
+
+
+    const todoChangeHandler = (event) => {
+        const doValue = event.target.value;
+        const newTodo = {
+            do: doValue
+        };
+        setTodo(newTodo);
+    }
+
+    return(
+        <>
+            <h3>Todos</h3>
+            <ul className="list-group">
+
+
+                <li className="list-group-item">
+                    <button onClick={createTodoClickHandler}
+                            className="btn btn-primary w-15
+                          float-end">
+                        Create</button>
+
+                    <input
+                        onChange={todoChangeHandler}
+                        value={todo.do}
+                        className="form-control w-75"/>
+                </li>
+
+                {
+
+                    todos.map((todo,index) =>
+                                  <li key={todo._id} className="list-group-item">
+                                      <input type="checkbox" checked={todo.done} onChange={() => toggleTodo(index)}/>
+                                      {todo.do}
+                                      <button onClick={() => deleteTodoClickHandler(index)}
+                                              className="btn btn-danger w-10
+                          float-end">
+                                          Delete</button>
+                                  </li>
+                    )
+                }
+            </ul>
+        </>
+    );
+};
+export default Todos;
